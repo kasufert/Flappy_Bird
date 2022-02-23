91df6b0d66282f6c7017a3a2f1af42cba1a976c0
Jack Simunic <jacksimunic2017@gmail.com>
2022-02-23 02:52:58 -0800

    Created bird script

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class BirdScript : MonoBehaviour
 {
+    private Rigidbody2D _rb;
+    private Vector2 _vel;
+    private const float GRAV = 9.81f;
+    private const float JUMP_SPEED = 4f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.isKinematic = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (!Mgr.Paused)
+		{
+            UpdateMovement();
+		}
+
+        void UpdateMovement()
+		{
+            var delTime = Time.deltaTime;
+            _vel.y -= GRAV * delTime;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+			{
+                _vel.y = JUMP_SPEED;
+			}
+
+            _rb.position += _vel * delTime;
+
+		}
     }
 }
